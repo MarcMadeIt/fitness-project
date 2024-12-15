e8abe46e9cd091d85b10a6b92259d82b8fc0ecbe
Marc Made It <spurdo.dk@gmail.com>
2024-12-15 17:43:07 +0100

    test

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: ['https://staystrong.vercel.app', 'http://localhost:5173'],
     credentials: true,
-    methods: ["POST"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
